27ccbc1e572a36dde83f1e984578eadad8123d5f
franciscozdo <zdobyl.frank@gmail.com>
2023-03-13 21:25:35 +0100

    Update vgit signs

--- plugin/vgit.lua
+++ plugin/vgit.lua
@@ -20,6 +20,57 @@ local vgit_options = {
     },
     settings = {
         live_blame = { enabled = false, },
+        signs = {
+            priority = 10,
+            definitions = {
+                GitSignsAddLn = {
+                    linehl = 'GitSignsAddLn',
+                    texthl = nil,
+                    numhl = nil,
+                    icon = nil,
+                    text = '',
+                },
+                GitSignsDeleteLn = {
+                    linehl = 'GitSignsDeleteLn',
+                    texthl = nil,
+                    numhl = nil,
+                    icon = nil,
+                    text = '',
+                },
+                GitSignsAdd = {
+                    texthl = 'GitSignsAdd',
+                    numhl = nil,
+                    icon = nil,
+                    linehl = nil,
+                    text = '+',
+                },
+                GitSignsDelete = {
+                    texthl = 'GitSignsDelete',
+                    numhl = nil,
+                    icon = nil,
+                    linehl = nil,
+                    text = '_',
+                },
+                GitSignsChange = {
+                    texthl = 'GitSignsChange',
+                    numhl = nil,
+                    icon = nil,
+                    linehl = nil,
+                    text = '~',
+                },
+            },
+            usage = {
+                screen = {
+                    add = 'GitSignsAddLn',
+                    remove = 'GitSignsDeleteLn',
+                },
+                main = {
+                    add = 'GitSignsAdd',
+                    remove = 'GitSignsDelete',
+                    change = 'GitSignsChange',
+                },
+            },
+        },
     },
 }
 
